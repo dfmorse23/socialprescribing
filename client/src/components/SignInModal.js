9bf13cce7bb0f1479435112d57ac05ca2768679a
Jonathan Warner <jewarner57@gmail.com>
2021-09-04 23:34:17 -0400

    feat: create username and password forms in sign in modal

--- client/src/components/SignInModal.js
+++ client/src/components/SignInModal.js
@@ -1,21 +1,25 @@
 import React, { useState } from 'react';
-import { Modal } from '@material-ui/core';
+import { Modal, FormControl, OutlinedInput, FormLabel, FormControlLabel, Grid, Checkbox, Link } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  modal: {
-    height: '100vh',
-    width: '100vw',
-    display: 'flex',
-    alignItems: 'center',
-  },
   paper: {
-    margin: '0 auto',
-    width: '400px',
+    margin: '100px auto',
+    width: '500px',
     backgroundColor: theme.palette.background.paper,
     borderRadius: '7px',
     boxShadow: theme.shadows[4],
-    padding: theme.spacing(2, 4, 3),
+    padding: theme.spacing(4, 10, 8),
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  formControl: {
+    margin: '20px 0',
+    width: '100%',
+  },
+  formLabel: {
+    margin: '10px 0'
   },
 }));
 
@@ -25,12 +29,40 @@ export default function SignInModal(props) {
   const [open, setOpen] = useState(false)
 
   const body = (
-    <div className={classes.modal}>
-      <div className={classes.paper}>
-        <p>Welcome back</p>
-        <h2>Login to your account.</h2>
-      </div>
-    </div>
+    <div className={classes.paper}>
+      <p>Welcome back</p>
+      <h2>Login to your account.</h2>
+      <form className={classes.root} noValidate autoComplete="off">
+        <FormControl className={classes.formControl} required="true" variant={"outlined"}>
+          <FormLabel className={classes.formLabel} htmlFor="email" shrink='false' name="email">Email</FormLabel>
+          <OutlinedInput id="email" aria-describedby="email address" />
+        </FormControl>
+
+        <FormControl className={classes.formControl} required="true" variant={"outlined"}>
+          <FormLabel className={classes.formLabel} htmlFor="password" shrink='false' name="password">Password</FormLabel>
+          <OutlinedInput id="password" aria-describedby="password" />
+        </FormControl>
+
+        <Grid container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Grid item>
+            <FormControlLabel
+              control={<Checkbox checked={false} onChange={() => { }} name="rememberMe" />}
+              label="Remember Me"
+            />
+          </Grid>
+
+          <Grid item>
+            <Link href="#" variant="body2">
+              Forgot Password?
+            </Link>
+          </Grid>
+        </Grid>
+      </form >
+    </div >
   );
 
   return (
@@ -44,9 +76,7 @@ export default function SignInModal(props) {
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
       >
-        <div onClick={() => setOpen(false)}>
-          {body}
-        </div>
+        {body}
       </Modal>
     </div>
   );
