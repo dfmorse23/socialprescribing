6309367e2773c2cc7a7126bde6ef7e4ad97dc1db
Jackie <taekwondo.chan@gmail.com>
2021-09-09 18:49:41 -0700

    scrapes volunteermatch successfully

--- server/volunteerScraper.js
+++ server/volunteerScraper.js
@@ -1,20 +1,81 @@
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+const cheerio = require("cheerio");
 puppeteer.use(StealthPlugin());
 
 // ---Puppeteer-extra + Stealth Plugins ---
 const scrapeVolunteerMatch = async (zipcode) => {
 	try {
-		const url = `https://www.volunteermatch.org/search/?l=${zipcode}`;
+		const endpoint = `https://www.volunteermatch.org/search/?l=${zipcode}`;
 		const browser = await puppeteer.launch();
 		const page = await browser.newPage();
 		await page.setJavaScriptEnabled(true);
-		await page.goto(url), { waituntil: "networkidle0" };
+		await page.goto(endpoint), { waituntil: "networkidle0" };
 		const pageHTML = await page.evaluate(() => document.querySelector("*").outerHTML);
+
+		const $ = cheerio.load(pageHTML);
+		const returnedData = [];
+
+		// For each list item
+		$("li.pub-srp-opps__opp").each((i, el) => {
+			let cardData = cheerio.load(el);
+			let title = "";
+			let url = "";
+			let location = "";
+			let time = "";
+
+			// Get titles & urls
+			cardData("div > h3 > a").each((i, el) => {
+				title = cardData(el)
+					.text()
+					.replace(/(\r\n|\n|\r)/gm, " ")
+					.trim();
+
+				url = `https://www.volunteermatch.org` + cardData(el).attr("href");
+			});
+
+			// Get locations
+			cardData("div.pub-srp-opps__info > div.pub-srp-opps__loc").each((i, el) => {
+				location = cardData(el).text();
+			});
+
+			// Get time/date
+			if (cardData("div.pub-srp-opps__info > div.pub-srp-opps__date.i-block").length > 0) {
+				cardData("div.pub-srp-opps__info > div.pub-srp-opps__date.i-block").each(
+					(i, el) => {
+						// Flexible Schedule
+						time = cardData(el)
+							.text()
+							.replace(/(\r\n|\n|\r)/gm, "")
+							.replace("|", "")
+							.trim();
+					}
+				);
+			} else {
+				cardData("div.pub-srp-opps__info > div.pub-srp-opps__date > div.i-block").each(
+					(i, el) => {
+						// Time & date
+						time = cardData(el)
+							.text()
+							.replace(/(\r\n|\n|\r)/gm, "")
+							.replace("|", "")
+							.trim();
+					}
+				);
+			}
+
+			returnedData.push({
+				title: title,
+				url: url,
+				location: location,
+				time: time,
+				tag: "dummy tag",
+			});
+		});
 
 		await browser.close();
 
-		return;
+		return returnedData;
 	} catch (err) {
 		return err;
 	}
