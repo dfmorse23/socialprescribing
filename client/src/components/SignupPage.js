0f7905551ca6337a42d921bba45a89c6fdb1be51
Jonathan Warner <jewarner57@gmail.com>
2021-09-12 11:42:18 -0400

    feat: improve navbar layout, add currentuser display in navbar

--- client/src/components/SignupPage.js
+++ client/src/components/SignupPage.js
@@ -8,10 +8,12 @@ import React from 'react';
 const useStyles = makeStyles((theme) => ({
   orb: {
     position: 'absolute',
-    width: '637px',
-    height: '637px',
+    width: '337px',
+    height: '337px',
+    top: '200px',
+    left: '300px',
     zIndex: '-1',
-    background: `linear-gradient(90deg, ${theme.palette.beige1} 0%, #FFFFFF 100%)`,
+    background: `linear-gradient(90deg, ${theme.palette.beige1} 50%, #FFFFFF 100%)`,
     filter: 'blur(100px)',
   },
 }));
