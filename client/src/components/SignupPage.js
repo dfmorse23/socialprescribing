5b15156a5c9118a9fdf6f31ff7a378a6db4bc623
Jonathan Warner <jewarner57@gmail.com>
2021-09-15 14:12:04 -0400

    feat: fix eslint warnings

--- client/src/components/SignupPage.js
+++ client/src/components/SignupPage.js
@@ -1,26 +1,11 @@
 import { Container, CssBaseline, Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 
 import Footer from './Footer';
 import Header from './Header';
 import SignupForm from './SignupForm';
 import React from 'react';
 
-const useStyles = makeStyles((theme) => ({
-  orb: {
-    position: 'absolute',
-    width: '337px',
-    height: '337px',
-    top: '200px',
-    left: '300px',
-    zIndex: '-1',
-    background: `linear-gradient(90deg, ${theme.palette.beige1} 50%, #FFFFFF 100%)`,
-    filter: 'blur(100px)',
-  },
-}));
-
 export default function SignupPage(props) {
-  const classes = useStyles();
 
   return (
     <React.Fragment>
