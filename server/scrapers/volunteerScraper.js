5989cd975ccbbceea5874e73d1a5e4f61969b3d1
Jackie <taekwondo.chan@gmail.com>
2021-09-17 11:00:46 -0700

    replace puppeteer with fetch

--- server/scrapers/volunteerScraper.js
+++ server/scrapers/volunteerScraper.js
@@ -1,88 +1,49 @@
-const puppeteer = require("puppeteer-extra");
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-const cheerio = require("cheerio");
-puppeteer.use(StealthPlugin());
+const fetch = require("node-fetch");
 
-// ---Puppeteer-extra + Stealth Plugins ---
 const getEvents = (zipcode) => {
 	return new Promise(async (resolve, reject) => {
-		try {
-			const endpoint = `https://www.volunteermatch.org/search/?l=${zipcode}`;
-			const browser = await puppeteer.launch();
-			const page = await browser.newPage();
-			await page.setJavaScriptEnabled(true);
-			await page.goto(endpoint);
-			const pageHTML = await page.evaluate(() => document.querySelector("*").outerHTML);
+		fetch("https://www.volunteermatch.org/s/srp/search", {
+			headers: {
+				accept: "application/json",
+				"accept-language": "en-US,en;q=0.9",
+				"content-type": "application/json",
+				"sec-ch-ua": '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+				"sec-ch-ua-mobile": "?0",
+				"sec-ch-ua-platform": '"macOS"',
+				"sec-fetch-dest": "empty",
+				"sec-fetch-mode": "cors",
+				"sec-fetch-site": "same-origin",
+			},
+			referrer: `https://www.volunteermatch.org/search/?l=${zipcode}`,
+			referrerPolicy: "strict-origin-when-cross-origin",
+			body: '{"query":"query {\\nsearchSRP(input:{\\nreturnVirtualAndOnSiteOpps: true\\nlocation: \\"94103\\"\\nvirtual: false\\ncategories: []\\nskills: []\\nradius: \\"20\\"\\ngreatFor: []\\nspecialFlag: \\"\\"\\nkeywords: []\\npageNumber: 1\\nsortCriteria: null\\nnumberOfResults: 25\\n}){\\nnumberOfResults\\nresultsSize\\noriginalResultSize\\ncurrentPage\\nsortCriteria\\nhasDistanceCriteria\\ntopCity\\ntotalVolunteersNeeded\\ncityLocation\\nsrpOpportunities{\\ndetail {\\ncategories\\nid\\nlocation {\\ncity\\ncountry\\npostalCode\\nregion\\nvirtual\\n}\\nparentOrg {\\nid\\nname\\n}\\nshifts {\\nid\\n}\\ntitle\\nurl\\n}\\ndateRange {\\nendDate\\nendTime\\nongoing\\nsingleDayOpps\\nstartDate\\nstartTime\\n}\\ndistance\\nparentOrgOppCount\\nplaintextDescription\\npostDate\\npremiumOrg\\n}}}","location":"94103","radius":"20"}',
+			method: "POST",
+			mode: "cors",
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				let returnedData = [];
+				opps = data["data"]["searchSRP"]["srpOpportunities"];
 
-			const $ = cheerio.load(pageHTML);
-			const returnedData = [];
-
-			// For each list item
-			$("li.pub-srp-opps__opp").each((i, el) => {
-				let cardData = cheerio.load(el);
-				let title = "";
-				let url = "";
-				let location = "";
-				let time = "";
-
-				// Get titles & urls
-				cardData("div > h3 > a").each((i, el) => {
-					title = cardData(el)
-						.text()
-						.replace(/(\r\n|\n|\r)/gm, " ")
-						.trim();
-
-					url = `https://www.volunteermatch.org` + cardData(el).attr("href");
-				});
-
-				// Get locations
-				cardData("div.pub-srp-opps__info > div.pub-srp-opps__loc").each((i, el) => {
-					location = cardData(el).text();
-				});
-
-				// Get time/date
-				if (
-					cardData("div.pub-srp-opps__info > div.pub-srp-opps__date.i-block").length > 0
-				) {
-					cardData("div.pub-srp-opps__info > div.pub-srp-opps__date.i-block").each(
-						(i, el) => {
-							// Detect no specific time/date
-							time = cardData(el)
-								.text()
-								.replace(/(\r\n|\n|\r)/gm, "")
-								.replace("|", "")
-								.trim();
-						}
-					);
-				} else {
-					cardData("div.pub-srp-opps__info > div.pub-srp-opps__date > div.i-block").each(
-						(i, el) => {
-							// Time & date
-							time = cardData(el)
-								.text()
-								.replace(/(\r\n|\n|\r|\|)/gm, "")
-								.replace(/(?<=[0-9]).*(?=\-)/, " ")
-								.trim();
-						}
-					);
+				for (let i = 0; i < opps.length; i++) {
+					returnedData.push({
+						title: opps[i]["detail"]["title"],
+						url: opps[i]["detail"]["url"],
+						location: opps[i]["detail"]["location"],
+						date: {
+							startDate: opps[i]["dateRange"]["startDate"],
+							endDate: opps[i]["dateRange"]["endDate"],
+						},
+						tag: "Volunteering",
+					});
 				}
 
-				returnedData.push({
-					title: title,
-					url: url,
-					location: location,
-					time: time,
-					tag: "Volunteering",
-				});
+				resolve(returnedData);
+			})
+			.catch((err) => {
+				reject(err.message);
 			});
-
-			browser.close();
-
-			resolve(returnedData);
-		} catch (err) {
-			reject(err);
-		}
 	});
 };
 
-module.exports = { getEvents: getEvents };
+getEvents("94103").then((res) => console.log(res));
