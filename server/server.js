ee5ca0f751a66e923c9680aebbe18167948e60c5
Jackie <taekwondo.chan@gmail.com>
2021-09-21 11:30:02 -0700

    broken cors policy

--- server/server.js
+++ server/server.js
@@ -9,6 +9,13 @@ const app = express();
 
 app.use(cors());
 app.use("/api/scrapers", scraper);
+app.use(cors());
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", ["*"]);
+	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+	next();
+});
 
 app.get("/", (req, res) => {
 	return res.json({ response: "Hello World" });
