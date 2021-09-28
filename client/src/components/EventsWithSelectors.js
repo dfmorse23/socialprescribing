07ac34aea76a42345d637b3fc7a855b3fe85d065
Jonathan Warner <jewarner57@gmail.com>
2021-09-28 15:07:09 -0400

    feat: display scraped data from server

--- client/src/components/EventsWithSelectors.js
+++ client/src/components/EventsWithSelectors.js
@@ -1,5 +1,4 @@
-import { useEffect, useRef, useState } from "react";
-
+import { useState } from "react";
 import EventCard from './EventCard';
 import FilterBar from './FilterBar';
 import { Grid } from '@material-ui/core';
@@ -7,7 +6,6 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import SearchWithGraphic from './SearchWithGraphic';
 import SearchIcon from '@material-ui/icons/Search';
-import get_dummy_data from './get_dummy_data';
 import EventSkeleton from './EventSkeleton'
 
 export default function EventsWithSelectors(props) {
@@ -16,8 +14,8 @@ export default function EventsWithSelectors(props) {
   const [userHasSearched, setUserHasSearched] = useState(false)
   const [eventSearchError, setEventSearchError] = useState()
   const [events, setEvents] = useState([]);
-  const [filterSelections, setFilterSelections] = useState(() => ["All"]);
-  let allEvents = useRef([]);
+  const [filterSelections, setFilterSelections] = useState(() => ['All']);
+  const [allEvents, setAllEvents] = useState()
 
   const handleSearch = async (searchValue) => {
     setUserHasSearched(true)
@@ -30,8 +28,10 @@ export default function EventsWithSelectors(props) {
         body: JSON.stringify({ searchValue }),
       })
 
-      const resJson = response.json()
-      setEvents(resJson)
+      const resJson = await response.json()
+      console.log([...resJson[0].EventBrite, ...resJson[1].Volunteering])
+      setAllEvents([...resJson[0].EventBrite, ...resJson[1].Volunteering])
+      setEvents([...resJson[0].EventBrite, ...resJson[1].Volunteering])
     }
     catch (err) {
       // Show a general search failed error to the user
@@ -49,7 +49,7 @@ export default function EventsWithSelectors(props) {
     // if there is nothing selected or all is selected display all events
     if (newSelections.length === 0 || difference.includes("All")) {
       newSelections = ["All"];
-      setEvents(allEvents.current);
+      setEvents(allEvents);
     } else {
       // remove 'All' from the selected filters if present
       const index = newSelections.indexOf("All");
@@ -58,18 +58,12 @@ export default function EventsWithSelectors(props) {
       }
 
       // filter for events that match the selected tags
-      const newEvents = allEvents.current.filter((event) => newSelections.includes(event.tag));
+      const newEvents = allEvents.filter((event) => newSelections.includes(event.tag));
       setEvents(newEvents);
     }
 
     setFilterSelections(newSelections);
   };
-
-  // initial render event
-  // useEffect(() => {
-  //   allEvents.current = get_dummy_data();
-  //   setEvents(allEvents.current);
-  // }, []);
 
   return (
     <React.Fragment>
