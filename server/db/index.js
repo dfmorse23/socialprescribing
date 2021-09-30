bf6f06f5b6f44fa138a428dedef65981ef8ba2be
Kou-kun42 <shinigamithekido@gmail.com>
2021-09-30 18:15:26 -0400

    Added base get route for favorites

---
+++ server/db/index.js
@@ -0,0 +1,32 @@
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+    user: process.env.AWS_USER,
+    host: process.env.AWS_HOST,
+    database: process.env.AWS_DB,
+    password: process.env.AWS_PASSWORD,
+    port: process.env.AWS_PORT,
+});
+
+module.exports = {
+    query: (text, params, callback) => {
+        const start = Date.now();
+        return pool.query(text, params, (err, res) => {
+            const duration = Date.now() - start;
+            console.log("executed query", {
+                text,
+                duration,
+                rows: res.rowCount,
+            });
+            callback(err, res);
+        });
+    },
+    getClient: (callback) => {
+        pool.connect((err, client, done) => {
+            callback(err, client, done);
+        });
+    },
+};
