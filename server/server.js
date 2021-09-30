2f50bf7d43e7214cda2f97b0efcedd2a8885de9a
Kou-kun42 <shinigamithekido@gmail.com>
2021-09-30 19:15:16 -0400

    Added get and add favorites routes

--- server/server.js
+++ server/server.js
@@ -4,17 +4,19 @@ var cors = require("cors");
 
 // Routes
 const scraper = require("./routes/scrapers.js");
+const database = require("./routes/database.js");
 
 const app = express();
 
 app.use(cors());
 app.use("/api/scrapers", scraper);
+app.use("/user", database);
 
 app.get("/", (req, res) => {
-	return res.json({ response: "Hello World" });
+    return res.json({ response: "Hello World" });
 });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-	console.log(`Server listening on ${PORT}`);
+    console.log(`Server listening on ${PORT}`);
 });
