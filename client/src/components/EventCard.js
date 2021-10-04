3b25a145263844f8b1a8a5c08b76ef5cd1fb83f4
Danika H Jaskulak-Gonzalez <danikaherminise@gmail.com>
2021-10-04 15:33:38 -0700

    added notes to outline my task

--- client/src/components/EventCard.js
+++ client/src/components/EventCard.js
@@ -1,5 +1,6 @@
 import { Box, Button, Grid, Typography, IconButton, CardActions } from '@material-ui/core';
 import { Card, CardActionArea, CardContent, CardMedia } from '@material-ui/core';
+// what is this border icon for? 
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import { useAuth } from '../contexts/AuthContext';
@@ -9,10 +10,23 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
+/*
+TO DO: 
+- inspect Airbnb's styling for their cards and search results 
+- look through Material UI's syntax on how to make the cardMedia into a card background
+  - possibly have to load images as props, check with backend to see if images are being
+    saved in json file with each prescription 
+  - then use useState to take in the images as objects to be used for each card 
+- add an icon to the card for the link to the site the card is referring to 
+- figure out why the card images' widths is depending on the title text
+- fix the flexbox to make the cards stay the same size despite the browser width changing 
+*/
+
 const useStyles = makeStyles((theme) => ({
   cardDetails: {
     flex: 1,
   },
+  /* search how to make this cardMedia the card background, like Airbnb's cards  */
   cardMedia: {
     paddingBottom: theme.spacing(2),
     maxHeight: 175,
@@ -91,7 +105,10 @@ export default function EventCard(props) {
             title="Contemplative Reptile"
           />
           <CardContent className={classes.eventCardTitle}>
+            {/* why are there two card media tags? */}
             <CardMedia component="img" image={"https://source.unsplash.com/1600x900/?nature,water"} title={event.title} className={classes.cardMedia} />
+            {/* what will happen if we seperate the cardMedia and the box component? will that 
+                fix the issue of the image width depending on the card's title? */}
             <Box
               component="div"
               classes={{ root: classes.customBox }}
@@ -104,6 +121,7 @@ export default function EventCard(props) {
         </CardActionArea>
         <CardActions className={classes.cardActionsContainer}>
           <Grid container justifyContent="space-around" alignItems="center">
+            {/* add icon for link to social prescription, instead of the whole card being a link */}
             <IconButton aria-label="add to favorites" onClick={() => handleLike()}>
               <FavoriteIcon />
             </IconButton>
