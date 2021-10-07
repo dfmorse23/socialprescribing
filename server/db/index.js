e094213851ab838aef1933c41354184b609b18af
Kou-kun42 <shinigamithekido@gmail.com>
2021-10-06 20:31:02 -0400

    Fixed bugs where favorites would become undefined, and implemented json parser

--- server/db/index.js
+++ server/db/index.js
@@ -21,12 +21,6 @@ module.exports = {
     query: (text, params, callback) => {
         const start = Date.now();
         return pool.query(text, params, (err, res) => {
-            const duration = Date.now() - start;
-            console.log("executed query", {
-                text,
-                duration,
-                rows: res.rowCount,
-            });
             callback(err, res);
         });
     },
