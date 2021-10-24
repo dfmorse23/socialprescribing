e0232a4cbc9a568fffe686b4af71d54ae04a9047
Hannah Le <hannah.lgbhan@gmail.com>
2021-10-24 19:47:11 -0400

    Fixed dev config

---
+++ server.compiled.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+var cors = require("cors"); // Routes
+
+
+const scraper = require("./routes/scrapers.js");
+
+const database = require("./routes/database.js");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/scrapers", scraper);
+app.use("/user", database);
+app.get("/", (req, res) => {
+  return res.json({
+    response: "Hello World"
+  });
+});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
