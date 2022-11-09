7f3f76bc057a02a519bda76827055f343ff809e9
Christian Thompson <cthompson@Christians-MacBook-Pro-2.local>
2022-11-09 15:48:45 -0800

    changed font

--- client/src/index.js
+++ client/src/index.js
@@ -1,52 +1,56 @@
-import './index.css';
+import "./index.css";
 
-import LandingPage from './components/LandingPage';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ThemeProvider } from '@material-ui/core/styles';
-import { createTheme } from '@material-ui/core/styles';
-import reportWebVitals from './reportWebVitals';
+import LandingPage from "./components/LandingPage";
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { createTheme } from "@material-ui/core/styles";
+import reportWebVitals from "./reportWebVitals";
 import { HashRouter, Route, Switch } from "react-router-dom";
-import { AuthProvider } from './contexts/AuthContext';
-import SignupPage from './components/SignupPage';
-import ForgotPasswordPage from './components/ForgotPasswordPage';
-import ErrorBoundary from './components/ErrorBoundary';
-import Error404Page from './components/Error404Page';
+import { AuthProvider } from "./contexts/AuthContext";
+import SignupPage from "./components/SignupPage";
+import ForgotPasswordPage from "./components/ForgotPasswordPage";
+import ErrorBoundary from "./components/ErrorBoundary";
+import Error404Page from "./components/Error404Page";
 
 // import Blog from './components/Blog';
 
 const theme = createTheme({
   palette: {
-    green1: '#17B793',
-    green2: '#A4E2B4',
-    green3: '#ACD4CC',
-    beige1: '#F3ECBF',
-    beige2: '#FAFAF2',
-    beige3: 'rgb(	251, 247, 225, 0.64)',
-    titleColor: '#049579',
+    green1: "#17B793",
+    green2: "#A4E2B4",
+    green3: "#ACD4CC",
+    beige1: "#F3ECBF",
+    beige2: "#FAFAF2",
+    beige3: "rgb(	251, 247, 225, 0.64)",
+    titleColor: "#049579",
   },
   typography: {
     fontFamily: [
-      'Quicksand',
-      'Helvetica'
-    ].join(','),
+      'Poppins',
+    ].join(","),
   },
 });
 
 ReactDOM.render(
-
   <React.StrictMode>
-    <ThemeProvider theme={theme} >
+    <ThemeProvider theme={theme}>
       <AuthProvider>
         <HashRouter>
           <ErrorBoundary theme={theme}>
             <Switch>
               <Route exact path="/" component={LandingPage} />
-              <Route exact path="/signin" component={() => (
-                <LandingPage signin={true} />
-              )} />
+              <Route
+                exact
+                path="/signin"
+                component={() => <LandingPage signin={true} />}
+              />
               <Route exact path="/signup" component={SignupPage} />
-              <Route exact path="/forgotpassword" component={ForgotPasswordPage} />
+              <Route
+                exact
+                path="/forgotpassword"
+                component={ForgotPasswordPage}
+              />
               {/* 404 page */}
               <Route path="*" component={Error404Page} />
             </Switch>
@@ -54,9 +58,8 @@ ReactDOM.render(
         </HashRouter>
       </AuthProvider>
     </ThemeProvider>
-  </React.StrictMode>
-  ,
-  document.getElementById('root')
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
