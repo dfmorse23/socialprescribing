b970778e0028ba6bff07322fa048c99cbff9093b
alexthegreat194 <alexander.gentry@aol.com>
2022-11-10 15:23:04 -0800

    setup

---
+++ server/routes/favorites.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const primsa = require("../../primsa/client");
+
+/**
+ * get user favorites
+ * add favorite
+ * remove favorite
+ * delete favorites?
+ */
+
+
+module.exports = router;
