7b26e90571a40528eb59f5be9d49c7b1d5f4e346
alexthegreat194 <alexander.gentry@aol.com>
2022-11-10 15:24:53 -0800

    get user favorites

--- server/routes/favorites.js
+++ server/routes/favorites.js
@@ -9,5 +9,18 @@ const primsa = require("../../primsa/client");
  * delete favorites?
  */
 
+router.get("/favorites/:user_uid", async (req, res) => {
+    const { user_uid } = req.params;
+    if (!user_uid) {
+        res.status(400).send({ message: "user_uid is required" });
+    }
+    const favorites = await primsa.user_favorites.findMany({
+        where: {
+            userId: user_uid,
+        },
+    });
+
+    res.send(favorites);
+});
 
 module.exports = router;
