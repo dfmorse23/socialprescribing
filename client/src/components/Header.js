f368a13399ddb65546406d744a635f1558651270
Christian Thompson <cthompson@Christians-MacBook-Pro-2.local>
2022-11-09 16:54:30 -0800

    updated colors to match landing page site

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,11 +6,13 @@ import { makeStyles } from '@material-ui/core/styles';
 import { useAuth } from '../contexts/AuthContext';
 
 const useStyles = makeStyles((theme) => ({
+  // Theme for toolbar
   toolbar: {
-    background: theme.palette.beige3,
+    background: theme.palette.backgroundcolor,
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4),
   },
+  // Theme for title 
   toolbarTitle: {
     flex: 1,
     fontWeight: 'bold',
@@ -22,11 +24,12 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'left',
     marginRight: theme.spacing(4),
   },
+  // theme for sign in button and signin hover
   toolbarAuthButton: {
-    backgroundColor: theme.palette.green1,
+    backgroundColor: theme.palette.bluePrimary,
     color: 'white',
     "&:hover": {
-      backgroundColor: '#17b08e'
+      backgroundColor: theme.palette.blueSecondary
     },
   }
 }));
