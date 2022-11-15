9225c2f285f3465c6560618804487c11c90bea52
alexthegreat194 <alexander.gentry@aol.com>
2022-11-14 17:28:31 -0800

    add favorite

--- server/routes/favorites.js
+++ server/routes/favorites.js
@@ -20,7 +20,47 @@ router.get("/favorites/:user_uid", async (req, res) => {
         },
     });
 
-    res.send(favorites);
+    return res.send(favorites);
+});
+
+router.post("/favorites/:user_uid", async (req, res) => {
+    const { user_uid } = req.params;
+    const { tag, url, date, title, location } = req.body;
+
+    if (!user_uid) {
+        res.status(400).send({ message: "user_uid is required" });
+    }
+
+    if (!tag || !url || !date || !title || !location) {
+        res.status(400).send({ message: "tag, url, date, title, location are required" });
+    }
+
+    const user = await prisma.user.findFirst({
+        where: {
+            id: user_uid,
+        },
+    });
+
+    if (!user) {
+        res.status(400).send({ message: "user does not exist" });
+    }
+
+    const favorite = await prisma.user_favorites.create({
+        data: {
+            tag,
+            url,
+            date,
+            title,
+            location,
+            userId: user.id,
+        },
+    });
+
+    return res.status(200).send({
+        message: "favorite added",
+        favorite,
+    });
+
 });
 
 module.exports = router;
