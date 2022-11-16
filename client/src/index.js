87d711c9a7a3570ef4520cd549f2d1bcab81c23c
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-11-15 14:26:42 -1000

    [edit] revamp stuff (buttons and title jumbotron thing)

--- client/src/index.js
+++ client/src/index.js
@@ -28,7 +28,7 @@ const theme = createTheme({
     blueSecondary: "#7B8CEC",
     backgroundColor: "#FAFAFA",
     titleColor: "#4361EE",
-    textColor: "#363C57"
+    textColor: "#363C57",
     greyPrimary: "#363C57",
     greyMedium: "#535874",
     greyLight: "#7F85A0",
