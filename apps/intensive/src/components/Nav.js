33e8e408e743866fdaecc097297f11a9605f0d10
Christian Thompson <cthompson@christiansmbp2.lan>
2023-01-04 12:25:32 -0800

    finished basic nav, will add more functionality later

--- apps/intensive/src/components/Nav.js
+++ apps/intensive/src/components/Nav.js
@@ -1,10 +1,33 @@
+import { Box, Button, ButtonGroup, Flex, Heading } from "@chakra-ui/react";
+import { ReactComponent as MainLogo } from "../images/logo.svg";
 import React from "react";
 
 const Nav = () => {
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <Flex p="5" w={"100%"} justify="space-around">
+      <Flex>
+        <MainLogo />
+        <Heading size={"md"} paddingLeft={2} fontWeight>
+          social prescribing usa
+        </Heading>
+      </Flex>
+      <Flex>
+        <ButtonGroup>
+          <Button size={"s"} variant={"link"} color="Text">
+            Home
+          </Button>
+          <Button size={"s"} variant={"link"} color="Text">
+            Find
+          </Button>
+          <Button size={"s"} variant={"link"} color="Text">
+            About
+          </Button>
+          <Button size={"s"} bg="primary" color={"white"} p={2}>
+            Login
+          </Button>
+        </ButtonGroup>
+      </Flex>
+    </Flex>
   );
-}
-export default Nav
+};
+export default Nav;
--- apps/intensive/src/pages/Home.js
+++ apps/intensive/src/pages/Home.js
@@ -1,10 +1,14 @@
+import { Box, } from "@chakra-ui/react";
 import React from "react";
+import Nav from "../components/Nav";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Box w={"100%"} h={"100vh"}>
+        <Nav />
+      </Box>
+    </>
   );
 };
 export default Home;
