93603677e54a6119dcf753dea36ce5bc5dedbad0
Elias <e.wambugu192@gmail.com>
2023-01-06 19:49:53 -0500

    added partial search logic

--- apps/intensive/src/components/SearchBar.js
+++ apps/intensive/src/components/SearchBar.js
@@ -4,18 +4,28 @@ import {
   InputGroup,
   InputLeftElement,
   InputRightElement,
+  Text,
 } from "@chakra-ui/react";
-import React from "react";
+import React, {  useState } from "react";
 import { Search, MapPin } from "react-feather";
 
 const SearchBar = () => {
-  
+  const [zipCode, setZipCode] = useState('');
+  const [error, setError] = useState('');
+
+  const onChange = (e) => {
+    if (!isNaN(e.target.value)) {
+      setError("");
+      setZipCode(e.target.value);
+    }
+  };
   return (
     <>
       <Flex
         w={["300px", "600px", "600px", "600px"]}
         h={"45px"}
         align={"center"}
+        flexDir='column'
       >
         <InputGroup shadow={"2xl"}>
           <InputLeftElement pointerEvents={"none"}>
@@ -26,11 +36,18 @@ const SearchBar = () => {
             variant="outline"
             bg="white"
             h={"45px"}
+            value={zipCode}
+            onChange={onChange}
           />
           <InputRightElement>
               <Search size={20} color={"#7F85A0"} />
           </InputRightElement>
         </InputGroup>
+        {error && (
+          <Text fontWeight={"500"} color="red.500">
+            {error}
+          </Text>
+        )}
       </Flex>
     </>
   );
