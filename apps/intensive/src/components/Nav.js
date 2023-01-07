e2b847e359ae909fedd635e195dae78111326d62
Christian Thompson <cthompson@christiansmbp2.lan>
2023-01-06 16:01:09 -0800

    made navbar responsive and made find button return you to the landing page

--- apps/intensive/src/components/Nav.js
+++ apps/intensive/src/components/Nav.js
@@ -4,15 +4,20 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 const Nav = () => {
   const navigate = useNavigate();
-  
+
   return (
-    <Flex p={10} w={"100%"} justify="space-around">
+    <Flex
+      p={[5, 10, 10, 10]}
+      w={["100%", "100%", "100%", "100%"]}
+      justify={["none", "space-around", "space-around", "space-around"]}
+      // bg="red"
+    >
       {/* Logo and Title Text -------------------------- */}
       <Flex align="center">
         {/* Logo SVG */}
         <MainLogo />
         <Heading
-          fontSize={"24px"}
+          fontSize={["15px", "24px", "24px", "24px"]}
           fontWeight={600}
           paddingLeft={2}
           color={"text"}
@@ -21,18 +26,26 @@ const Nav = () => {
         </Heading>
       </Flex>
       {/* Nav Buttons -------------------------- */}
-      <Flex color={"Text"}>
+      <Flex color={"Text"} fontSize={["12px"]} ml={2}>
         <ButtonGroup size={"s"} fontWeight={""}>
-          <Button variant={"link"} color={"text"}>
-            Home
-          </Button>
-          <Button variant={"link"} color={"text"}>
+          <Button
+            variant={"link"}
+            color={"text"}
+            onClick={() => navigate("/")}
+          >
             Find
           </Button>
-          <Button  variant={"link"} color={"text"}>
+          <Button variant={"link"} color={"text"}>
             About
           </Button>
-          <Button onClick={() => navigate('/login')} bg="primary" color={"white"} p={2}>
+          <Button
+            onClick={() => navigate("/login")}
+            bg="primary"
+            color={"white"}
+            p={2}
+            // mt={'1'}
+            // mb={'1'}
+          >
             Sign In
           </Button>
         </ButtonGroup>
